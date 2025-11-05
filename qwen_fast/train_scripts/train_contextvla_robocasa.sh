325d6aa48beaa073be5062dcd4936025c4e32486
Huiwon Jang <huiwoen0516@kaist.ac.kr>
2025-11-05 15:27:15 +0900

    .
    Removed WANDB_API_KEY export from script.

--- qwen_fast/train_scripts/train_contextvla_robocasa.sh
+++ qwen_fast/train_scripts/train_contextvla_robocasa.sh
@@ -2,8 +2,6 @@
 
 source ~/anaconda3/bin/activate activate contextvla
 
-export WANDB_API_KEY=42c81e07cdc22dbc10cab806306a5112e1016262
-
 torchrun --standalone --nnodes=1 --nproc_per_node=8 --master_port=19002 scripts/train_contextvla.py contextvla_robocasa \
     --exp-name=contextvla_robocasa \
     --num-train-steps=30000 \
